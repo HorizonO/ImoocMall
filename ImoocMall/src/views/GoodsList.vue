9dd827d9df0566b490d11020a1d263d8a5dabefe
HorizonCxl <xlinasaplife@163.com>
2020-11-19 21:59:00 +0800

    #imoocmall

--- ImoocMall/src/views/GoodsList.vue
+++ ImoocMall/src/views/GoodsList.vue
@@ -1,20 +1,73 @@
 <template>
-<div>
-  <p>这是商品列表页面 Hello</p>
-  <span>{{$route.params.goodsId}}</span><br>
-  <span>{{$route.params.name }}</span>
-  <router-link to="/goods/title">show goods title.</router-link>
-  <router-link to="/goods/img">show goods image.</router-link>
   <div>
-    <router-view></router-view>
-  </div>
+    <nav-header></nav-header>
+   <nav-bread>
+     <span>Goods</span>
+   </nav-bread>
+    <div class="accessory-result-page accessory-page">
+      <div class="container">
+        <div class="filter-nav">
+          <span class="sortby">Sort by:</span>
+          <a href="javascript:void(0)" class="default cur">Default</a>
+          <a href="javascript:void(0)" class="price">Price <svg class="icon icon-arrow-short"><use xlink:href="#icon-arrow-short"></use></svg></a>
+          <a href="javascript:void(0)" class="filterby stopPop">Filter by</a>
+        </div>
+        <div class="accessory-result">
+          <!-- filter -->
+          <div class="filter stopPop" id="filter">
+            <dl class="filter-price">
+              <dt>Price:</dt>
+              <dd><a href="javascript:void(0)">All</a></dd>
+              <dd>
+                <a href="javascript:void(0)">0 - 100</a>
+              </dd>
+            </dl>
+          </div>
 
-</div>
+          <!-- search result accessories list -->
+          <div class="accessory-list-wrap">
+            <div class="accessory-list col-4">
+              <ul>
+                <li>
+                  <div class="pic">
+                    <a href="#"><img src="static/img/1.jpg" alt=""></a>
+                  </div>
+                  <div class="main">
+                    <div class="name">XX</div>
+                    <div class="price">XX</div>
+                    <div class="btn-area">
+                      <a href="javascript:;" class="btn btn--m">加入购物车</a>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <nav-footer></nav-footer>
+
+  </div>
 </template>
 
 <script>
+import './../assets/css/base.css'
+import './../assets/css/product.css'
+import NavHeader from '../components/NavHeader.vue'
+import NavFooter from "../components/NavFooter";
+import NavBread from "../components/NavBread";
 export default {
-  name: "GoodsList"
+ data(){
+   return{
+     msg:"Hello Vue"
+   }
+ },
+  components:{
+    NavHeader,
+    NavFooter,
+    NavBread,
+  }
 }
 </script>
 
